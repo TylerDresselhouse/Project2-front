fc34b0739ed03347f39ceae671a4742ea49109e4
James Holzer <james.holzer1990@gmail.com>
2018-01-18 14:34:30 -0500

    Test push

---
+++ Project2-front/src/models/asbuser.model.ts
@@ -0,0 +1,11 @@
+export class AsbUser {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+
+    constructor(id: number, username: string, password: string, firstName: string, lastName: string) {
+        
+    }
+}
