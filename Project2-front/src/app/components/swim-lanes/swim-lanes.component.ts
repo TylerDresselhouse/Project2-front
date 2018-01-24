5ef20b34c18da6a31a41c3cb961915e2e8684225
Tyler <ttdresselhouse@quinnipiac.edu>
2018-01-24 09:14:20 -0500

    Adding basic swim lane view

--- Project2-front/src/app/components/swim-lanes/swim-lanes.component.ts
+++ Project2-front/src/app/components/swim-lanes/swim-lanes.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swim-lanes.component.css']
 })
 export class SwimLanesComponent implements OnInit {
+  lane1 = ['card1', 'card2', 'card3'];
+  lane2 = [];
+  lane3 = ['card4'];
+  lane4 = ['card5', 'card6'];
+  swimLanes = [this.lane1, this.lane2, this.lane3, this.lane4]; // this is where we would grab all the lanes from the DB
 
   constructor() { }
 
