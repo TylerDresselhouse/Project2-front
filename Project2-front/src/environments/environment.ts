4ef3be1d6ab825bb9c4dede2a0a16fded8321cd5
unknown <dylantbritton@gmail.com>
2018-01-27 20:34:27 -0500

    almost done with card delete

--- Project2-front/src/environments/environment.ts
+++ Project2-front/src/environments/environment.ts
@@ -24,6 +24,8 @@ export const environment = {
   },
 
   card: {
-    save: (swimLaneId: number) => `${context}createCard/${swimLaneId}`
+    save: (swimLaneId: number) => `${context}createCard/${swimLaneId}`,
+
+    delete: (cardId: number) =>  `${context}delete/card/${cardId}`
   }
 };
